f38b5524fc831f6dc5a44c5cf17ba91ebf3a2890
Bozhidar Zashev <bzashev@users.noreply.github.com>
2023-03-19 13:32:56 +0200

    UI development

--- script.js
+++ script.js
@@ -1,58 +1,26 @@
+//
+// Minesweeper Script file
+//
 
-// document / window
-//First developer Viktor
-//Second developer: Nikola
-//third row
-//forth row
-let bodyEl;
-let divEl;
-let linkEl;
-let containerEl;
-
-function addDiv() {
-    let addedDivEl = document.createElement("div");
-    containerEl.appendChild(addedDivEl);
-}
-
-function init() {
-    bodyEl = document.getElementById("body");
-    divEl = document.getElementById("div");
-    linkEl = document.getElementById("link");
-    containerEl = document.getElementById("container");
-
-
-    bodyEl.addEventListener("click", function (event) {
-        if (event.target.tagName.toLowerCase() === "div") {
-            console.log("div clicked");
-        }
-    });
-
-    divEl.addEventListener("click", function (event) {
-        console.log("div clicked");
-    });
-
-
-
-    linkEl.addEventListener("click", function (event) {
-        //Add div to container
-        event.preventDefault();
-        event.stopPropagation();
-        addDiv();
-    });
-
-    window.addEventListener("resize", function (e) {
-        console.log(e);
-
-    });
-
-}
-
-let myEvent = new Event("boz", {title:"My event"});
-let myEvent2 = new Event("boz2", {title:"My event 2"});
-
-document.dispatchEvent(myEvent);
 
 window.addEventListener("load", (event) => {
-    init();
+    //body.addEventListener("click", () => {
+    //    var allTileEl = document.querySelectorAll(".cell-tile");
+    //    allTileEl.forEach((el) => {
+    //        if (el.classList.contains("rock-type")) {
+    //            el.classList.replace("rock-type", "crystal-type");
+    //        }
+    //        else if (el.classList.contains("crystal-type")) {
+    //            el.classList.replace("crystal-type", "lava-type");
+    //        }
+    //        else if (el.classList.contains("lava-type")) {
+    //            el.classList.replace("lava-type", "dungeon-type");
+    //        }
+    //        else if (el.classList.contains("dungeon-type")) {
+    //            el.classList.replace("dungeon-type", "rock-type");
+    //        }
+            
+    //    })
+    //})
 });
 
