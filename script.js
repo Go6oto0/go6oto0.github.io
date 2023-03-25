2c75aa3be167254a21504b5ce1023f7a4335eb9f
Виктор Зашев <viktor@webvella.com>
2023-03-25 13:31:54 +0200

    Initial script implementation - may have bugs

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 //
 // Minesweeper Script file
 //
+var x = 10;
+var y = 10;
+var minesCount = 30;
+var undiscoveredMines = minesCount;
+var minefield = [];
+var lockGame = false;
+
+function cell(row, column)
+{
+    var selector = 'div[data-x="' + column + '"][data-y="' + row + '"]';
+    var cellObj = {};
+    cellObj.content = '<div class="cell" data-x="' + column + '" data-y="' + row + '"><span class="nearMines">0</span><span class="mine"></span><span class="flag"></span></div>';
+    cellObj.isMine = false;
+    cellObj.isRevealed = false;
+    cellObj.isFlagged = false;
+    cellObj.nearMines = 0;
+    cellObj.cellType = "normal"; // "sidesOnly"
+    cellObj.x = column;
+    cellObj.y = row;
+    cellObj.visited = false;
+    cellObj.value = document.querySelector(selector);
+    //cellObj.isInfected = false;
+    //cellObj.hasImunity = false;
+    cellObj.hover = function (isHovered)
+    {
+        if (isHovered) {
+            value.addClass("hover");
+        } else {
+            value.removeClass("hover");
+        }
+    };
+}
 
 
+
+/*
 window.addEventListener("load", (event) => {
     //body.addEventListener("click", () => {
     //    var allTileEl = document.querySelectorAll(".cell-tile");
@@ -24,3 +58,4 @@ window.addEventListener("load", (event) => {
     //})
 });
 
+*/
